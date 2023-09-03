e283a9a9726d5ca07c584dfa058dba196ae03a02
Charles <babouwitonze921@gmail.com>
2023-09-03 21:29:09 +0200

    Finally done the todo app 🥹

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,130 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <body>
+    <img
+      class="background-img"
+      src="../public/assets/todo/bg-desktop-light.jpg"
+      alt="background"
+      id="background"
+    />
+    <div class="wrapper">
+      <div class="title">
+        TODO
+        <img
+          class="switch"
+          id="switch"
+          src="../public/assets/todo/icon-moon.svg"
+          @click="changeTheme"
+          alt="theme"
+          :style="{ transform: 'rotate(' + rotateDegrees + 'deg)' }"
+        />
+      </div>
+      <div class="inputField">
+        <button type="radio" @click="addToList" class="radioButton"></button>
+        <input placeholder="Add task" type="text" name="" id="input" />
+      </div>
+      <div class="todoList">
+        <div class="list">
+          <div
+            class="todo"
+            @mouseover="handleHover"
+            @mouseleave="handleLeave"
+            v-for="(todo, index) in toDoList"
+            :key="index"
+          >
+            <input
+              type="checkbox"
+              @click="complete(index)"
+              :checked="todo.completed"
+            />
+            <p :class="{ underline: todo.completed }">{{ todo.text }}</p>
+            <img
+              class="clear"
+              @click="remove(index)"
+              src="../public/assets/todo/icon-cross.svg"
+              alt=""
+            />
+          </div>
+        </div>
+
+        <div class="footer">
+          <div class="bt-left">{{ toDoList.length }} items left</div>
+          <div class="options">All Active Completed</div>
+          <div @click="clear" class="bt-rigth">Clear Completed</div>
+        </div>
+      </div>
+    </div>
+  </body>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+const toDoList = JSON.parse(localStorage.getItem("todo")) || [];
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      theme: "light",
+      toDoList: toDoList,
+      rotateDegrees: 0,
+    };
+  },
+  methods: {
+    changeTheme() {
+      document.body.classList.toggle("dark-mode");
+      if (this.theme === "light") {
+        this.theme = "dark";
+        document.getElementById("background").src =
+          "/assets/todo/bg-desktop-dark.jpg";
+        document.getElementById("switch").src = "/assets/todo/icon-sun.svg";
+        this.rotateDegrees += 360;
+      } else {
+        this.theme = "light";
+        document.getElementById("background").src =
+          "/assets/todo/bg-desktop-light.jpg";
+        document.getElementById("switch").src = "/assets/todo/icon-moon.svg";
+        this.rotateDegrees += 360;
+      }
+    },
+    addToList() {
+      const text = document.getElementById("input").value;
+      const todo = {
+        text,
+        completed: false,
+      };
+      this.toDoList.push(todo);
+      console.log(this.toDoList);
+      document.getElementById("input").value = "";
+      localStorage.setItem("todo", JSON.stringify(this.toDoList));
+    },
+    complete(index) {
+      this.toDoList[index].completed = !this.toDoList[index].completed;
+      console.log(this.toDoList[index].completed);
+
+      localStorage.setItem("todo", JSON.stringify(this.toDoList));
+    },
+    remove(index) {
+      this.toDoList.splice(index, 1);
+      localStorage.setItem("todo", JSON.stringify(this.toDoList));
+    },
+    test(e) {
+      console.log(e.target, e);
+    },
+    handleHover(e) {
+      if (e.target.children[2]) {
+        e.target.children[2].style.display = "block";
+      }
+    },
+    handleLeave(e) {
+      if (e.target.children[2]) {
+        e.target.children[2].style.display = "none";
+      }
+    },
+    clear() {
+      localStorage.clear();
+      this.toDoList = [];
+      // window.location.reload();
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style src="../src/styles.css"></style>
